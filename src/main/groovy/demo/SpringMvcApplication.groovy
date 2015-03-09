1f4c5543dea38d0691575d8a231bb83e13b7bb09
Ron Kurr <rkurr@transparent.com>
2015-03-09 23:01:03 +0000

    Initial checking

---
+++ src/main/groovy/demo/SpringMvcApplication.groovy
@@ -0,0 +1,12 @@
+package demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringMvcApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run SpringMvcApplication, args
+    }
+}
