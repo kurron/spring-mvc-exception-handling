6bea581c0ffcfeb567090556208f463ff354a8f4
Ron Kurr <rkurr@transparent.com>
2015-03-09 23:28:34 +0000

    Can fail the GET on demand.

--- src/main/groovy/demo/SimpleController.groovy
+++ src/main/groovy/demo/SimpleController.groovy
@@ -2,6 +2,7 @@ package demo
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -10,9 +11,16 @@ import org.springframework.web.bind.annotation.RestController
 @RestController( '/' )
 class SimpleController {
 
-    @RequestMapping( method = RequestMethod.GET )
-    ResponseEntity<String> handleGet() {
-        new ResponseEntity<String>( 'GET successful', HttpStatus.OK )
+    @RequestMapping( value = '/{id}', method = RequestMethod.GET )
+    ResponseEntity<String> handleGet( @PathVariable( 'id' ) String id ) {
+        ResponseEntity<String> responseEntity
+        if ( id.toLowerCase().contains( 'fail' ) ) {
+            responseEntity = new ResponseEntity<String>( 'GET forced to fail', HttpStatus.INTERNAL_SERVER_ERROR )
+        }
+        else {
+            responseEntity = new ResponseEntity<String>( 'GET successful', HttpStatus.OK )
+        }
+        responseEntity
     }
 
     @RequestMapping( method = RequestMethod.POST )
