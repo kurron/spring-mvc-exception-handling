c09e2a32ab071cdf071530c920e78ad02234f47e
Ron Kurr <rkurr@transparent.com>
2015-03-09 23:22:58 +0000

    Initial POST working.

--- src/main/groovy/demo/SimpleController.groovy
+++ src/main/groovy/demo/SimpleController.groovy
@@ -2,6 +2,7 @@ package demo
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -12,5 +13,10 @@ class SimpleController {
     @RequestMapping( method = RequestMethod.GET )
     ResponseEntity<String> handleGet() {
         new ResponseEntity<String>( 'GET successful', HttpStatus.OK )
+    }
+
+    @RequestMapping( method = RequestMethod.POST )
+    ResponseEntity<String> handlePost( @RequestBody byte[] payload ) {
+        new ResponseEntity<String>( 'POST successful', HttpStatus.CREATED )
     }
 }
